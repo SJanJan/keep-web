5076186c2916232aba9fadf40ce2ebfd64e85b75
lijian <812284688@qq.com>
2019-10-21 18:26:54 +0800

    添加视频

---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="keep-header position-fixed">
+    <div>
+      <video id="video-bg"
+             src="@/assets/mp4/5s.mp4"
+             muted
+             autoplay
+             loop></video>
+    </div>
+
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.keep-header {
+  width: 100%;
+}
+#video-bg {
+  width: 100%;
+  object-fit: contain;
+}
+</style>
---
+++ src/layout/AppNavbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-navbar variant="faded"
+              type="light">
+      <b-navbar-brand href="#">
+        <img src="@/assets/logo.png"
+             class="d-inline-block align-top"
+             alt="Kitten">xxxx
+      </b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
