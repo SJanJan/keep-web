6e36208eea25639cccbb792be8afcb6e02196863
lijian <812284688@qq.com>
2020-01-22 10:26:50 +0800

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:sjanjan/keep-web.git master:gh-pages
+git push -f git@github.com:sjanjan/keep-web.git master:default
 
 cd -
